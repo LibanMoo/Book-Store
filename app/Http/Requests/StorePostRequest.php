aa71d084fb3a0619d73a92a07a59a76be135f6bf
LibanMoo <libanmohamedreal@gmail.com>
2025-05-27 00:11:19 +0300

    create: made formRequestValidation

---
+++ app/Http/Requests/StorePostRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'isbn'=> 'required|integer|max:15',
+            'name'=> 'required|string|max:255',
+            'name'=> 'required|string|max:255'
+        ];
+    }
+}
