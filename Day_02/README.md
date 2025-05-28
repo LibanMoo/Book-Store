ac5c4dcdc0b653b933607ced90c71fa13293f0da
LibanMoo <libanmohamedreal@gmail.com>
2025-05-28 22:07:38 +0300

    cloned the last code

---
+++ Day_02/README.md
@@ -0,0 +1,40 @@
+# Book Store
+
+## 📌 Overview
+A web applicatio that allows the users to view, create, update and delete books
+
+## 🎯 Purpose
+This is part of challenge i am udergoing
+
+## 🛠️ Technologies Used
+- [Laravel]
+- [Blade]
+- [MySql]
+- [Vite]
+
+## 📚 What I Learned
+- How to use Factories and faker method in Laravel
+- how to work with blade
+- How to debug errors
+- Migrations
+
+## 🚀 How to Run the Project
+1. Clone the repo:
+   ```bash
+   git clone https://github.com/LibanMoo/Book-Store.git
+2. Navigate into the project directory:
+   ```bash
+       cd BookStore
+3. install the dependencies:
+   ```bash
+       npm install
+
+2. Start the project:
+   ```bash
+       php artisan serve
+
+
+
+🖼️ Screenshots
+[screenshots](./screenshots/)
+
---
+++ Day_02/app/Http/Controllers/BookStoreController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BookStore;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Http\Request;
+
+class BookStoreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $bookStore = BookStore::paginate(10);
+
+        return view('BookStore.index', [
+            'BookStore'=> $bookStore
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('BookStore.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePostRequest $request)
+    {
+         $validated = $request->validated();
+
+          BookStore::create($validated);
+
+          return redirect()->route('bookstore.index')->with('success', 'Post created!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BookStore $bookStore)
+    {
+        return view('BookStore.show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Request $request, BookStore $bookstore)
+    {
+        return view('BookStore.edit', compact('bookstore'));
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(StorePostRequest $request, BookStore $bookstore)
+    {
+        $validated = $request->validated();
+
+          $bookstore->update($validated);
+
+          return redirect()->route('bookstore.index')->with('success', 'Post Updated!');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BookStore $bookstore)
+    {
+        $bookstore->delete();
+        return redirect()->route('bookstore.index')->with('success', 'Post Deleted!');
+    }
+}
