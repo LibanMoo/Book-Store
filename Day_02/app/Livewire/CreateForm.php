e10b06fcbef911de0b370b330fb1ffd397c83b46
LibanMoo <libanmohamedreal@gmail.com>
2025-05-30 00:23:16 +0300

    create: stored the input data in variables and declared the rules

--- Day_02/app/Livewire/CreateForm.php
+++ Day_02/app/Livewire/CreateForm.php
@@ -3,11 +3,35 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\models\BookStore;
 
 class CreateForm extends Component
 {
     public function render()
     {
         return view('livewire.create-form');
+    }
+    public $isbn;
+    public $name;
+    public $author;
+    public $description;
+ 
+    protected $rules = [
+        'isbn' => 'required|min:6',
+        'name' => 'required|min:10',
+        'author' => 'required|min:5',
+        'description' => 'required|min:10'
+    ];
+ 
+    public function updated($propertyName)
+    {
+        $this->validateOnly($propertyName);
+    }
+ 
+    public function saveCreateForm()
+    {
+        $validatedData = $this->validate();
+ 
+        BookStore::create($validatedData);
     }
 }
