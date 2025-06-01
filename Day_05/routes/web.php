bf18753d607cf903a8673d5fc967308daaf93869
LibanMoo <libanmohamedreal@gmail.com>
2025-06-01 14:54:57 +0300

    initial update

--- Day_05/routes/web.php
+++ Day_05/routes/web.php
@@ -15,9 +15,6 @@ Route::get('/', function () {
 Route::resource('bookstore', BookStoreController::class);
 
 
-Route::middleware(['auth', 'role:admin'])->prefix('admin')->group(function () {
-    Route::resource('BookStoreResource', BookStoreController::class);
-});
 
 // Route::get('/{id}/edit', [BookStoreController::class, 'edit'])->name('edit');
 
